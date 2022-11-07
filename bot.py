d9f52f2c7798fe10e639d102d012ffbf468e7acf
Dennis <ddhuynh@clemson.edu>
2022-11-06 21:17:10 -0500

    putting bot into main funct

--- bot.py
+++ bot.py
@@ -1,32 +1,38 @@
 import requests
 from flask import Flask
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/', methods=['GET'])
+    def home():
+        return 'https://my-groupme-bot.onrender.com/'
+
+    @app.route('/', methods=['POST'])
+    def receive():
+        print("Incoming Msg: ")
+        print(data)
+
+        # Prevent self-reply
+        if data['sender_type'] != 'bot':
+            if data['text'].startswith('/ping'):
+                send(data['name'] + ' pinged me!')
+
+        return 'ok', 200
+
+    def send(msg):
+        url = "https://api.groupme.com/v3/bots/post"
+        data = {
+            "bot_id": os.getenv("BOT_ID"),
+            "text": msg
+        }
+        req = requests.post(url, data=data)
+        print(req)
+
+    return app
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return 'https://my-groupme-bot.onrender.com/'
-
-
-@app.route('/', methods=['POST'])
-def receive():
-    print("Incoming Msg: ")
-    print(data)
-
-    # Prevent self-reply
-    if data['sender_type'] != 'bot':
-        if data['text'].startswith('/ping'):
-            send(data['name'] + ' pinged me!')
-
-    return 'ok', 200
-
-
-def send(msg):
-    url = "https://api.groupme.com/v3/bots/post"
-    data = {
-        "bot_id": os.getenv("BOT_ID"),
-        "text": msg
-    }
-    req = requests.post(url, data=data)
-    print(req)
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
