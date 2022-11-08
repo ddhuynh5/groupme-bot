50589a14136852776cb34b08dc9f4583cd76bd76
Dennis <ddhuynh@clemson.edu>
2022-11-07 22:12:38 -0500

    testing img

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ token = os.getenv("TOKEN")
 bot_id = os.getenv("BOT_ID")
 app = Flask(__name__)
 
+url = "https://api.groupme.com/v3/bots/post"
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -22,17 +24,31 @@ def receive():
     if data['sender_type'] != 'bot':
         if data['text'].startswith('/ping'):
             send(data['name'] + ' pinged me!')
-        if data['name'] == "Dennis Huynh":
-            send('hi dennis')
+        if data['name'] == "Dennis Huynh" and data['text'].startswith('ayo'):
+            send_with_img('hi', 'ayo.jpg')
 
     return 'ok', 200
 
 
 def send(msg):
-    url = "https://api.groupme.com/v3/bots/post"
     json = {
         "bot_id": bot_id,
         "text": msg
+    }
+    req = requests.post(url, json=json)
+    print("request: ", req)
+
+
+def send_with_img(msg, img):
+    json = {
+        "bot_id": bot_id,
+        "text": msg,
+        "attachments": [
+            {
+                "type": "image",
+                "url": img
+            }
+        ]
     }
     req = requests.post(url, json=json)
     print("request: ", req)
