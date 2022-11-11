5c8d3c04929457af52a63ecaad72fb7e0acdbc07
Dennis <ddhuynh@clemson.edu>
2022-11-11 00:45:28 -0500

    cat

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-import requests
 import os
+import json
+import requests
 from flask import Flask, request
 
 token = os.getenv("TOKEN")
@@ -29,6 +30,10 @@ def receive():
             send("ok")
         if data["text"].startswith("ayo"):
             send("The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.")
+        if data["text"].startswith("cat"):
+            send_cat()
+        else:
+            send(":)")
 
     return "ok", 200
 
@@ -42,7 +47,16 @@ def send(msg):
     print("request: ", req)
 
 
-def post_img_to_groupme(msg, img):
+def send_cat():
+    url = "https://api.thecatapi.com/v1/images/search"
+    response = requests.get(url)
+    res = response.json()
+
+    for r in res:
+        requests.post(url, data=r["url"])
+
+
+def post_img_to_groupme():
     data = open("./mat.jpeg", "rb").read()
     res = requests.post(url="https://image.groupme.com/pictures",
                         data=data,
@@ -51,7 +65,7 @@ def post_img_to_groupme(msg, img):
     print(res.content)
 
 
-    """ def post_img_to_chat(img):
+def post_img_to_chat(img):
     post_data = {"text": "TESTING TEST TEST RAHHHH", "picture_url":YOUR_PIC_URL}
 
-    requests.post("https://api.groupme.com/v3/bots/post", params = post_params, data = post_data) """
+    requests.post("https://api.groupme.com/v3/bots/post", params = post_params, data = post_data)
